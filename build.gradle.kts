c0cb91d1f52927af61c67b1a6a60b2e9880243b6
cssxsh <cssxsh@gmail.com>
2022-05-22 10:33:16 +0800

    build: 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
 
-    id("net.mamoe.mirai-console") version "2.10.2"
+    id("net.mamoe.mirai-console") version "2.11.0"
     id("net.mamoe.maven-central-publish") version "0.7.1"
 }
 
@@ -15,6 +15,7 @@ mavenCentralPublish {
     licenseFromGitHubProject("AGPL-3.0", "master")
     publication {
         artifact(tasks.getByName("buildPlugin"))
+        artifact(tasks.getByName("buildPluginLegacy"))
     }
 }
 
@@ -30,11 +31,11 @@ dependencies {
         exclude("com.google.auto.service")
     }
     compileOnly("com.google.auto.service:auto-service:1.0.1")
-    compileOnly("net.mamoe:mirai-core-utils:2.10.2")
-    compileOnly("me.him188:kotlin-jvm-blocking-bridge-runtime-jvm:2.0.0-160.3")
+    compileOnly("net.mamoe:mirai-core-utils:2.11.0")
+    compileOnly("me.him188:kotlin-jvm-blocking-bridge-runtime-jvm:2.0.0-162.1")
 
-    testImplementation(kotlin("test", "1.6.0"))
-    runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation(kotlin("test", "1.6.21"))
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.36")
 }
 
 kotlin {
