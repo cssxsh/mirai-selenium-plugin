6e3cc31686ad20d7adef793497ea77b68070d664
cssxsh <cssxsh@gmail.com>
2024-02-17 16:36:29 +0800

    feat: dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
 
-    id("net.mamoe.mirai-console") version "2.16.0-RC"
+    id("net.mamoe.mirai-console") version "2.16.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
-    id("me.him188.kotlin-jvm-blocking-bridge") version "2.2.0-180.1"
+    id("me.him188.kotlin-jvm-blocking-bridge") version "3.0.0-180.1"
 }
 
 group = "xyz.cssxsh.mirai"
@@ -28,22 +28,22 @@ repositories {
 
 dependencies {
     api("com.github.jknack:handlebars:4.3.1")
-    api("org.seleniumhq.selenium:selenium-java:4.10.0")
-    implementation("org.apache.commons:commons-compress:1.23.0")
+    api("org.seleniumhq.selenium:selenium-java:4.16.1")
+    implementation("org.apache.commons:commons-compress:1.24.0")
     implementation("org.tukaani:xz:1.9")
     testImplementation(kotlin("test"))
     testImplementation("org.icepear.echarts:echarts-java:1.0.7")
     //
-    implementation(platform("net.mamoe:mirai-bom:2.16.0-RC"))
+    implementation(platform("net.mamoe:mirai-bom:2.16.0"))
     testImplementation("net.mamoe:mirai-logging-slf4j")
     testImplementation("net.mamoe:mirai-console-compiler-common")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.11"))
     testImplementation("org.slf4j:slf4j-simple")
     testImplementation("org.slf4j:jcl-over-slf4j:2.0.7")
     testImplementation("org.slf4j:jul-to-slf4j:2.0.7")
     //
-    implementation(platform("io.netty:netty-bom:4.1.96.Final"))
+    implementation(platform("io.netty:netty-bom:4.1.107.Final"))
 }
 
 kotlin {
